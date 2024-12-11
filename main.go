e766851b60ccc08bc0b254b5eb863e4ac64f2bce
mara <mara@the-witch.ru>
2024-12-11 19:54:49 +0300

    alt screen mode

--- main.go
+++ main.go
@@ -210,7 +210,7 @@ func (m model) monthMap() Month {
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
